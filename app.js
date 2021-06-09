24acba039502007b245c2554e992d4125d5d5055
Vilmos Misota <misotavilmos@gmail.com>
2021-06-10 00:06:25 +0100

    add scroll animations

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const statements = document.querySelectorAll(".statements");
-const description = document.querySelector(".description p");
-
-let options = {
-  rootMargin: "-45%",
-  treshold: 0.0,
-};
+const form = document.querySelector("form");
+const descriptionDesign = document.querySelector(".description-design");
+const description = document.querySelector(".description");
+const descriptionPharagraph = document.querySelector(".mission");
 
 const animateStatements = () => {
-  let observer = new IntersectionObserver(showItem, options);
+  let optionsStatement = {
+    rootMargin: "-45%",
+    treshold: 0.0,
+  };
+
+  let observer = new IntersectionObserver(showItem, optionsStatement);
 
   function showItem(entries) {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("active");
+        entry.target.classList.add("active-statements");
       }
     });
   }
@@ -22,20 +25,46 @@ const animateStatements = () => {
   });
 };
 
-const animateDescription = () => {
-  let observer = new IntersectionObserver(showItem, options);
+const animateForm = () => {
+  let optionsForm = {
+    rootMargin: "-20%",
+    treshold: 0.0,
+  };
+
+  let observer = new IntersectionObserver(showItem, optionsForm);
 
   function showItem(entries) {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("active-description");
-        console.log("hello");
+        entry.target.classList.add("active-form");
       }
     });
   }
 
-  observer.observe(description);
+  observer.observe(form);
+};
+
+const animateDescriptionDesign = () => {
+  let optionsDescription = {
+    rootMargin: "-40%",
+    treshold: 0.0,
+  };
+
+  let observer = new IntersectionObserver(showItem, optionsDescription);
+
+  function showItem(entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active-description-design");
+        description.classList.add("active-description");
+        descriptionPharagraph.classList.add("active-mission");
+      }
+    });
+  }
+
+  observer.observe(descriptionDesign);
 };
 
 animateStatements();
-// animateDescription();
+animateForm();
+animateDescriptionDesign();
